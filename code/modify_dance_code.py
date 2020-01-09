8a7e0f3e733c117a68a317fdbe4edd9f5a7869b4
ThaNyneTray <dtuiyot@gmail.com>
2020-01-08 19:41:59 -0500

    Added search functionality. Can search by name, category, tags, or description, or by everything.

--- code/modify_dance_code.py
+++ code/modify_dance_code.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtSql import *
-import sys
+import sys, time
 from code.dance_app_uis.modify_dance_window import Ui_ModifyDanceMove
 
 
@@ -45,12 +45,15 @@ class ModifyDanceWindow(QtWidgets.QMainWindow):
         self.db = create_connection()
 
         self.model = QSqlTableModel(db=self.db)
+        self.modelV2 = QSqlTableModel(db=self.db)
+        initialize_model(self.modelV2)
         initialize_model(self.model)
 
         self.ui.moves_View.setModel(self.model)
         self.ui.moves_View.setSortingEnabled(True)
         self.ui.submit_Button.clicked.connect(self.push_to_db)
         self.ui.cancel_Button.clicked.connect(self.cancel_changes)
+        self.ui.search_lineEdit.textChanged.connect(self.search)
 
     def push_to_db(self):
         # TODO: pop a confirmation dialog
@@ -58,6 +61,28 @@ class ModifyDanceWindow(QtWidgets.QMainWindow):
 
     def cancel_changes(self):
         self.model.revertAll()
+
+    # Implementing search.
+    # We take advantage of the qsqltable model - setFilter + filter_combobox
+    def search(self):
+        # we're using setFilter to implement search.
+        pattern = self.ui.search_lineEdit.text()
+        search_filter = self.ui.filter_comboBox.currentText()
+        if pattern is None or pattern == "":
+            self.model.setFilter("")
+        elif search_filter == "Everything":
+            self.model.setFilter(("name LIKE '%{0}%' "
+                                  "OR category LIKE '%{0}%' "
+                                  "OR tags LIKE '%{0}%'"
+                                  "OR description LIKE '%{0}%'".format(pattern)))
+        elif search_filter == "Name":
+            self.model.setFilter(("name LIKE '%{0}%'".format(pattern)))
+        elif search_filter == "Category":
+            self.model.setFilter(("category LIKE '%{0}%'".format(pattern)))
+        elif search_filter == "Tags":
+            self.model.setFilter(("tags LIKE '%{0}%'".format(pattern)))
+        elif search_filter == "Description":
+            self.model.setFilter(("description LIKE '%{0}%'".format(pattern)))
 
 
 app = QtWidgets.QApplication(sys.argv)
